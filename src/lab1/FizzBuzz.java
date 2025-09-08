8f372889d753018bfdaf25aa95b736f51951152d
StevenZhu12345 <yisteven.zhu@mail.utoronto.ca>
2025-09-08 17:12:27 -0400

    newer fixing

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -8,34 +8,35 @@ class FizzBuzz {
     public static void main(String[] args) {
         int i = 1;
         while(i < 100){
-            i = dodizzbuzz(i);
+            i = doFizzBuzz(i);
         }
     }
 
-    private static int dodizzbuzz(int i) {
+    public static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
 
-        // Print our appropriate result.
-        if (divisibleBy3 && divisibleBy5) {
+            // Print our appropriate result.
+            if (divisibleBy3 && divisibleBy5) {
 
-            System.out.println("Fizz Buzz");
+                System.out.println("Fizz Buzz");
 
-        } else if (divisibleBy3) {
+            } else if (divisibleBy3) {
 
-            System.out.println("Fizz");
+                System.out.println("Fizz");
 
-        } else if (divisibleBy5) {
+            } else if (divisibleBy5) {
 
-            System.out.println("Buzz");
+                System.out.println("Buzz");
 
-        } else {
+            } else {
 
-            System.out.println(i);
+                System.out.println(i);
 
-        }
-        i++;
+            }
+            i++;
         return i;
     }
-}
+    }
+
